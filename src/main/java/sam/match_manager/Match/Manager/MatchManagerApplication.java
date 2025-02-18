7fc88122fdec6acae5858eb4a9f5a9a0e675fdbe
Samuel Morais <SamuelMoraisdRS>
2025-02-18 10:14:07 -0300

    Initial Commit

---
+++ src/main/java/sam/match_manager/Match/Manager/MatchManagerApplication.java
@@ -0,0 +1,13 @@
+package sam.match_manager.Match.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sam/match_manager/Match/Manager/MatchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.match_manager.Match.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
