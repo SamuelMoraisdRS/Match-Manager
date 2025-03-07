ed1a7fc7139a56436e40194464e87fc1d33803d6
SamuelMorais <samuelmorais552@gmail.com>
2025-03-07 17:40:35 -0300

    feat: JoinMatchMessage record implementation

--- src/main/java/sam/match_manager/Match/Manager/messages/JoinMatchMessage.java
+++ src/main/java/sam/match_manager/Match/Manager/messages/JoinMatchMessage.java
@@ -1,10 +1,17 @@
 package sam.match_manager.Match.Manager.messages;
 
+import sam.match_manager.Match.Manager.enums.PlayerStatus;
 import sam.match_manager.Match.Manager.representations.Player;
 
 public record JoinMatchMessage(
   String matchCode,
-  Player playerData
+  // TODO: Use a player record object
+  String clientId,
+  PlayerStatus playerStatus,
+  int numberOfGuesses,
+  int numberOfCorrects,
+  int checksUsed,
+  int submitsUsed
 ) {
 
 }
